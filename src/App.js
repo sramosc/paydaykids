98896aa0efa6f3ef8d2fdd4fd8a45c37f91fc9a3
Sergio <you@example.com>
2019-04-10 21:02:18 +0200

    mas cambios login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import GoogleLogin from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
 //import Main from "./Main"
 
 const responseGoogle = (response) => {
@@ -9,13 +10,20 @@ const responseGoogle = (response) => {
 class App extends Component {
   render() {
     return (
-      <GoogleLogin
-        clientId="591884413778-r8amvm1i48ucfu7lvdmingu5ahjd0ka6.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
+      <div>
+        <GoogleLogin
+          clientId="591884413778-r8amvm1i48ucfu7lvdmingu5ahjd0ka6.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+        <GoogleLogout
+          buttonText="Logout"
+          onLogoutSuccess={responseGoogle}
+        >
+        </GoogleLogout>
+      </div>
     )
   }
 }
