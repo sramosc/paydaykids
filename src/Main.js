92402323f396a438a4590939d00ca6ae8214863b
Sergio <you@example.com>
2019-04-10 20:36:43 +0200

    inicial

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Main extends React.Component {
+    render(){
+        return (
+            <div>
+              <h1>Simple SPA</h1>
+              <ul className="header">
+                <li><a href="/">Home</a></li>
+                <li><a href="/stuff">Stuff</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+              <div className="content">
+                 
+              </div>
+            </div>
+        )
+}
+}
+
+export default Main
+
