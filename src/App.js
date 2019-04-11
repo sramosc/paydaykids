693723f067eb3e5d8e502f15945f366875f995c7
sergio.ramos <sergio.ramos@entelgydigital.com>
2019-04-11 11:08:19 +0200

    cambios

--- src/App.js
+++ src/App.js
@@ -1,56 +1,73 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
+import config from './config.json';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = { isAuthenticated: false, user: null, token: '' };
-  }
+    constructor() {
+        super();
+        this.state = { isAuthenticated: false, user: null, token: ''};
+    }
 
-  logout = () => {
-    this.setState({ isAuthenticated: false, token: '', user: null })
-  };
+    logout = () => {
+        this.setState({isAuthenticated: false, token: '', user: null})
+    };
 
-  googleResponse = (e) => { };
+    onFailure = (error) => {
+        alert(error);
+        console.log(error)
+    };
 
-  onFailure = (error) => {
-    alert(error);
-  }
+    googleResponse = (response) => {
+        const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+        const options = {
+            method: 'POST',
+            body: tokenBlob,
+            mode: 'cors',
+            cache: 'default'
+        };
+        fetch('http://localhost:4000/api/v1/auth/google', options).then(r => {
+            const token = r.headers.get('x-auth-token');
+            r.json().then(user => {
+                if (token) {
+                    this.setState({isAuthenticated: true, user, token})
+                }
+            });
+        })
+    };
 
-  render() {
+    render() {
     let content = !!this.state.isAuthenticated ?
-      (
-        <div>
-          <p>Authenticated</p>
-          <div>
-            {this.state.user.email}
-          </div>
-          <div>
-            <button onClick={this.logout} className="button">
-              Log out
-            </button>
-          </div>
-        </div>
-      ) :
-      (
-        <div>
-          <GoogleLogin
-            clientId="591884413778-r8amvm1i48ucfu7lvdmingu5ahjd0ka6.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={this.googleResponse}
-            onFailure={this.googleResponse}
-          />
-        </div>
-      );
+            (
+                <div>
+                    <p>Authenticated</p>
+                    <div>
+                        {this.state.user.email}
+                    </div>
+                    <div>
+                        <button onClick={this.logout} className="button">
+                            Log out
+                        </button>
+                    </div>
+                </div>
+            ) :
+            (
+                <div>
+                    <GoogleLogin
+                        clientId={config.GOOGLE_CLIENT_ID}
+                        buttonText="Login"
+                        onSuccess={this.googleResponse}
+                        onFailure={this.onFailure}
+                    />
+                </div>
+            );
 
-    return (
-      <div className="App">
-        {content}
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                {content}
+            </div>
+        );
+    }
 }
 
 export default App;
